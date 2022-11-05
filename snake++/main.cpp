cecffe26f73f7e82f2107df8d6f5b2de55ae106b
DillyzThe1 <dyl1029an@gmail.com>
2022-11-05 14:27:05 -0500

    window consistency and apple spawning

--- snake++/main.cpp
+++ snake++/main.cpp
@@ -5,11 +5,24 @@
 using namespace std;
 using namespace sf;
 
+const int defwinwidth = 480, defwinheight = 480, width = 24, height = 24;
+
+int getrand() {
+	srand(time(NULL));
+	return rand();
+}
+
+int rand_int(int min, int max) {
+	srand(rand() * time(NULL) * rand() * 0.5);
+	int num = rand() % (max - min);
+	return num + min;
+}
+
 int main() {
 	srand(time(0));
 	cout << "C++ is cool.";
 
-	RenderWindow window(VideoMode(480, 480), "Snake++ by DillyzThe1");
+	RenderWindow window(VideoMode(defwinwidth, defwinheight), "Snake++ by DillyzThe1");
 	window.setFramerateLimit(120);
 
 	Texture icon;
@@ -21,6 +34,11 @@ int main() {
 	else
 		cout << "Icon loading FAIL!";
 
+	RectangleShape apple;
+	apple.setFillColor(Color::Color(172, 50, 50, 255));
+	apple.setPosition(rand_int(0, width) * (defwinwidth / width), rand_int(0, height) * (defwinwidth / height));
+	apple.setSize(Vector2f::Vector2(defwinwidth / width, defwinwidth / height));
+
 	while (window.isOpen()) {
 		Event e;
 		while (window.pollEvent(e)) {
@@ -30,9 +48,18 @@ int main() {
 			if (e.type == Event::KeyPressed)
 				if (e.key.code == Keyboard::Escape)
 					window.close();
+				else if (e.key.code == Keyboard::Enter)
+					apple.setPosition(rand_int(0, width) * (defwinwidth / width), rand_int(0, height) * (defwinwidth / height));
+
+			if (e.type == Event::Resized)
+			{
+				int avgAmount = (window.getSize().x + window.getSize().y) / 2;
+				window.setSize(Vector2u::Vector2(avgAmount, avgAmount));
+			}
 		}
 
-		window.clear(Color::Color(175, 175, 175, 255));
+		window.clear(Color::Color(34, 32, 52, 255));
+		window.draw(apple);
 		window.display();
 	}
 
